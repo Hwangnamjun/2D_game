f01e009b23277e4cd12f0dde331db43da512c7d4
Hwangnamjun <dudrn0585@naver.com>
2022-05-16 15:14:19 +0900

    #3 player 및 enemy 추가
    player 및 enemy 추가

---
+++ Assets/Enemy.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    Animator animator;
+
+    public float Health
+    {
+        set 
+        {
+            health = value;
+
+            if (health <= 0)
+            {
+                Defeated();
+            }
+        }
+        get 
+        {
+            return health;
+        }
+    }
+
+    public float health = 1;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Defeated()
+    {
+        animator.SetTrigger("Defeated");
+    }
+
+    public void RemoveEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/SwordAttack.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    Vector2 rightAttackOffset;
+    public Collider2D swordCollder;
+    public float damage = 3;
+
+    private void Start()
+    {
+        rightAttackOffset = transform.position;
+    }
+    public void AttackRight()
+    {
+        swordCollder.enabled = true;
+        transform.localPosition = rightAttackOffset;
+    }
+    public void AttackLeft()
+    {
+        swordCollder.enabled = true;
+        transform.localPosition = new Vector3(rightAttackOffset.x * -1, rightAttackOffset.y);
+    }
+    public void StopAttack()
+    {
+        swordCollder.enabled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+     if(other.tag == "Enemy")
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+
+            if(enemy != null)
+            {
+                enemy.Health -= damage;
+            }
+        }
+    }
+}
